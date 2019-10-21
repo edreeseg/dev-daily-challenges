fac7228feab5eb5527d28981ad4953a8e5f502c0
Ed Reeseg <edward.reeseg@gmail.com>
2019-10-21 02:34:46 +0000

    Correcting code - find out if there's a better way to determine expected length of result without iterating through argument string first.

--- camelCase/camelCase.c
+++ camelCase/camelCase.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-#include <string.h>
 #include <ctype.h>
 
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
@@ -10,27 +9,33 @@ int main(int argc, char *argv[])
         return 1;
     }
     char *c = argv[1];
-    char result[strlen(argv[1])]; // Will end up taking up more space than necessary - come back and fix
+    int len = 0;
+    for (int i = 0; c[i] != '\0'; i++)
+    {
+        if (c[i] != ' ')
+        {
+            len++;
+        }
+    }
+    char result[len + 1];
     int wordStart = 0, counter = 0;
     while(*c != '\0')
     {
         if (*c == ' ')
         {
             wordStart = 1;
-            c++;
         }
         else if (wordStart)
         {
             wordStart = 0;
             result[counter++] = toupper(*c);
-            c++;
         }
         else
         {
             wordStart = 0;
             result[counter++] = tolower(*c);
-            c++;
         }
+        c++;
     }
     result[counter] = '\0';
     printf("%s\n", result);
